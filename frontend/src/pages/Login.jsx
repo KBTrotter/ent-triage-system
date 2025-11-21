0a70d8386111838cb2fa2b1aeb286c0254545578
KBTrotter <ktrotter@nevada.unr.edu>
2025-11-21 13:54:18 -0800

    updated login to use formik and yup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,131 +1,173 @@
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import React from "react";
 import { deepPurple } from "@mui/material/colors";
-import { Button, Box, Card, CardContent, TextField, Typography, FormControl, InputLabel, InputAdornment, OutlinedInput, IconButton} from "@mui/material";
+import {
+  Button,
+  Box,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
+} from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
+
 export default function Login() {
-    const { login } = useAuth();
-    const navigate = useNavigate(); 
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Username is required"),
+      password: Yup.string().required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      console.log(values);
+      await new Promise((r) => setTimeout(r, 500));
+      login(values.username, values.password);
+      navigate("/dashboard");
+      setSubmitting(false);
+      resetForm();
+    },
+  });
 
-    const [username, setUsername] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
 
-    const [usernameError, setUsernameError] = React.useState(false);
-    const [passwordError, setPasswordError] = React.useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
 
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleForgotPassword = () => {
+    console.log("Forgot Password clicked");
+  };
 
-    // For testing purposes only
-    // const handleAdminLogin = () => {
-    //     login("adminUser", "admin");
-    //     console.log("Admin user logged in");
-    //     navigate("/dashboard");
-    // };
-    
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-    const handleLogin = (u) => {
-        let hasError = false;
-        //check for empty fields -> error if empty
-        if (username.trim() === "") {
-            setUsernameError(true);
-            hasError = true;
-        } else {
-            setUsernameError(false);
-        }
-        if(password.trim() === "") {
-            setPasswordError(true);
-            hasError = true;
-        } else {
-            setPasswordError(false);
-        }
-        if (hasError) {
-            return;
-        }
-        //user login
-        login(username, password);
-        navigate("/dashboard");
-        console.log("Login button clicked");
-    };
+  // For testing purposes only
+  // const handleAdminLogin = () => {
+  //     login("adminUser", "admin");
+  //     console.log("Admin user logged in");
+  //     navigate("/dashboard");
+  // };
 
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: deepPurple[50],
+      }}>
+      <Card sx={{ width: 350, padding: 2 }}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            textAlign: "center",
+            padding: 2,
+          }}>
+          <Typography variant="h5" fontWeight="bold" sx={{ padding: 2 }}>
+            Login
+          </Typography>
+          {/* Username */}
+          <form onSubmit={formik.handleSubmit}>
+            <TextField
+              name="username"
+              label="Username"
+              variant="outlined"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+              fullWidth
+            />
+            {/* Password */}
+            <FormControl variant="outlined" fullWidth sx={{ mt: 2 }}>
+              <InputLabel
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }>
+                Password
+              </InputLabel>
 
-    return (
-        <Box sx={{
-                height: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                backgroundColor: deepPurple[50],
-            }}>
-            <Card sx={{ width: 350, padding: 2 }}>
-                <CardContent sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: 2,
-                        textAlign: "center",
-                        padding: 2
-                    }}>
-                    <Typography variant="h5" fontWeight="bold" sx={{padding: 2}}>Login</Typography>
-                    {/* Username */}
-                    <TextField 
-                      label="Username" 
-                      variant="outlined" 
-                      value={username} 
-                      onChange={(e) => setUsername(e.target.value)} 
-                      error={usernameError} 
-                      helperText={usernameError ? "Username is required": ""}
-                    />
+              <OutlinedInput
+                name="password"
+                type={showPassword ? "text" : "password"}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={
+                  formik.touched.password && Boolean(formik.errors.password)
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
 
-                    {/* Password */}
-                    <FormControl  variant="outlined">
-                      <InputLabel error={passwordError}>Password</InputLabel>
-                      <OutlinedInput
-                        type={showPassword ? 'text' : 'password'}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        error={passwordError} 
-                        endAdornment={
-                          <InputAdornment position="end">
-                            <IconButton
-                              onClick={handleClickShowPassword}
-                              edge="end"
-                            >
-                              {showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                          </InputAdornment>
-                        }
-                        label="Password"
-                      />
-                      {passwordError && (
-                        <Typography variant="caption" color="error"  sx={{ marginTop: "4px", textAlign: "left" , marginLeft: "14px"}}>
-                          Password is required
-                        </Typography>
-                      )}
-                    </FormControl> 
-                    {/* Login Buttons*/}
-                    <Button onClick={handleLogin} 
-                      sx={{
-                        backgroundColor: deepPurple[500],
-                        "&:hover": { backgroundColor: deepPurple[700] },
-                        color: "#fff",
-                        borderRadius: 3,
-                      }}>
-                        Log In
-                      </Button>
-                    <Button
-                      variant="text" 
-                      disableRipple 
-                      sx={{ 
-                        textTransform: "none", 
-                        "&:hover": { backgroundColor: "transparent", color: deepPurple[800] }, 
-                        color: deepPurple[400] 
-                      }}
-                      onClick={() => console.log("Forgot Password clicked")}>Forgot Password?</Button>
-                    {/* This is for admin role testing purposes only
-                    <button onClick={handleAdminLogin}>Login as Admin</button>*/}
-                </CardContent>
-            </Card>
-        </Box>
-    );
+              {formik.touched.password && formik.errors.password && (
+                <Typography
+                  variant="caption"
+                  color="error"
+                  sx={{
+                    marginTop: "3px",
+                    marginLeft: "12px",
+                    textAlign: "left",
+                  }}>
+                  {formik.errors.password}
+                </Typography>
+              )}
+            </FormControl>
+
+            <Button
+              type="submit"
+              sx={{
+                backgroundColor: deepPurple[500],
+                "&:hover": { backgroundColor: deepPurple[700] },
+                color: "#fff",
+                borderRadius: 3,
+                mt: 2,
+                width: "100%",
+                textTransform: "none",
+              }}>
+              Log In
+            </Button>
+            <Button
+              variant="text"
+              disableRipple
+              onClick={handleForgotPassword}
+              sx={{
+                textTransform: "none",
+                "&:hover": {
+                  backgroundColor: "transparent",
+                  color: deepPurple[800],
+                },
+                color: deepPurple[400],
+                mt: 2,
+              }}>
+              Forgot Password?
+            </Button>
+          </form>
+          {/* This is for admin role testing purposes only */}
+          {/* <button onClick={handleAdminLogin}>Login as Admin</button> */}
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
